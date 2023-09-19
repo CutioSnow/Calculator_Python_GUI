3fc299a95e75bc5c6e84a6a6754a840cf75495d3
snow <sshort95@gmail.com>
2023-09-19 10:48:49 -0400

    Initial Design

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import tkinter.ttk as ttk
 #Initializes tkinter window
 root = tk.Tk()
 
+#Configure grid weight for relative design
+root.grid_rowconfigure(0,weight=1)
+root.grid_rowconfigure(1,weight=1)
+
 #Creates calculator text label
 lbl_calculator_display = tk.Label(height=2, bg="white")
-lbl_calculator_display.pack(fill=tk.X)
+lbl_calculator_display.grid(row=0,column=0)
 
 #Frame acting as the master frame for the calculator button layout
 frm_calc_btn_layout = ttk.Frame(root)
-frm_calc_btn_layout.pack()
+frm_calc_btn_layout.grid(row=1,column=0)
+
+
 
 #Array defining calculator button layout
 calc_btn_set = [["%", "7", "8", "9", "÷"],
@@ -21,9 +27,13 @@ calc_btn_set = [["%", "7", "8", "9", "÷"],
 #TODO: Stylize button layout
 #Generates a grid of buttons within the button layout master frame
 for i in range(len(calc_btn_set)):
+    frm_calc_btn_layout.grid_rowconfigure(i,weight=1) #Allows row to use relative layout
     for j in range(len(calc_btn_set[i])):
+        frm_calc_btn_layout.grid_columnconfigure(j, weight=1) #Allows columns to use relative layout
+
+        #Creates button at specified grid location based on the calculator layout
         button = ttk.Button(frm_calc_btn_layout,text=calc_btn_set[i][j])
-        button.grid(row=i,column=j,padx=10,pady=10)
+        button.grid(row=i,column=j,sticky="nsew")
 
 #Runs tkinter event loop
 root.mainloop()
