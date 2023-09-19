d02bea8998e2e1de2123658c12e8ac5857673978
snow <sshort95@gmail.com>
2023-09-19 00:11:15 -0400

    Basic framework

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import tkinter.ttk as ttk
 root = tk.Tk()
 
 #Creates calculator text label
-solution = tk.Label(height=2, width=50, bg="white")
-solution.pack()
+lbl_calculator_display = tk.Label(height=2, bg="white")
+lbl_calculator_display.pack(fill=tk.X)
 
-#Frame acting as a grid container for buttons
-frame = ttk.Frame(root)
-frame.pack()
+#Frame acting as the master frame for the calculator button layout
+frm_calc_btn_layout = ttk.Frame(root)
+frm_calc_btn_layout.pack()
 
-#Creates 4x4 grid of buttons
-for i in range(4):
-    for j in range(4):
-        button = ttk.Button(frame,text=f"{i+1}x{j+1}")
+#Array defining calculator button layout
+calc_btn_set = [["%", "7", "8", "9", "÷"],
+                ["+/-", "4", "5", "6", "X"],
+                ["C", "1", "2", "3", "-"],
+                ["AC", "0", ".", "=", "+"]]
+
+#TODO: Stylize button layout
+#Generates a grid of buttons within the button layout master frame
+for i in range(len(calc_btn_set)):
+    for j in range(len(calc_btn_set[i])):
+        button = ttk.Button(frm_calc_btn_layout,text=calc_btn_set[i][j])
         button.grid(row=i,column=j,padx=10,pady=10)
 
 #Runs tkinter event loop
